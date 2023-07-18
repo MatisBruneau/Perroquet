54addef4dc68bd1789f789b4007a95021aafaad3
Matis Bruneau <98561485+MatisBruneau@users.noreply.github.com>
2023-07-18 16:15:07 +0200

    Update server.R

--- myshinyapp/inst/app/server.R
+++ myshinyapp/inst/app/server.R
@@ -1,41 +1,50 @@
-
-server <- function(input, output) {
-
-  data <- reactive({
-    # Connect to the DB
-    conn <- DBI::dbConnect(
-      RPostgres::Postgres(),
-      dbname = Sys.getenv("POSTGRESQL_DB_NAME"),
-      host = Sys.getenv("POSTGRESQL_DB_HOST"),
-      port = Sys.getenv("POSTGRESQL_DB_PORT"),
-      user = Sys.getenv("POSTGRESQL_DB_USER"),
-      password = Sys.getenv("POSTGRESQL_DB_PASSWORD")
-    )
-    
-    # Get the data
-    quakes <- DBI::dbGetQuery(conn, glue::glue("SELECT * FROM quakes WHERE mag >= {input$magSlider}"))
-    quakes <- data.frame(quakes)
-    
-    # Disconnect from the DB
-    DBI::dbDisconnect(conn)
-    
-    return(quakes)
-  })
-  
-  # Base map
-  output$map <- leaflet::renderLeaflet({
-    mymap <- leaflet::leaflet()
-    mymap <- leaflet::addTiles(mymap)
-    mymap <- leaflet::setView(mymap, lng=178,lat=-23,zoom=4)
-    mymap <- leaflet::addProviderTiles(mymap, leaflet::providers$Esri.WorldStreetMap)
-  })
-  
-  # Update markers
-  observe({
-    proxy <- leaflet::leafletProxy("map")
-    proxy <- leaflet::clearMarkers(proxy)
-    if (nrow(data()) > 0) {
-    proxy <- leaflet::addMarkers(proxy, data=data(), ~long, ~lat, label = ~mag)  
-    }
+server <- function(input, output){
+  observeEvent(input$submit_info, {
+    if (input$paragraphe == "Chômage"){
+      observeEvent(input$submit_info, {
+        source("myshinyapp/R/generateurs.R")
+        output$graphique1 <- renderPlotly(
+          generateur_figure4(as.numeric(input$annee), as.numeric(input$trimestre), as.numeric(input$horizon)))
+        output$graphique2 <- renderPlotly(NULL)
+        output$commentaires <- renderText({
+          generateur_chômage(input$annee, input$trimestre)})
+        output$tableau <- renderTable({
+          generateur_table_chomage(as.numeric(input$annee), as.numeric(input$trimestre), max(as.numeric(input$horizon),2))}, caption = "Taux de chômage au sens du BIT - Ensemble")
+      })
+    }else if (input$paragraphe == "Emploi"){
+      observeEvent(input$submit_info, {
+        source("myshinyapp/R/generateurs.R")
+        output$graphique1 <- renderPlotly({
+          ggplotly(generateur_figure2(as.numeric(input$annee), as.numeric(input$trimestre), as.numeric(input$horizon)))
+          })
+        output$graphique2 <- renderPlotly({
+          ggplotly(generateur_figure3(as.numeric(input$annee), as.numeric(input$trimestre), as.numeric(input$horizon)))
+        })
+        output$commentaires <- renderText({
+          generateur_emplois(input$annee, input$trimestre)})
+        output$tableau <- renderTable({
+          generateur_table_emploi(as.numeric(input$annee), as.numeric(input$trimestre), max(as.numeric(input$horizon),2))}, caption="Nombre d'emplois salariés en fin de trimestre")
+      })
+    }else if (input$paragraphe == "Demandeurs d'emploi") {
+      observeEvent(input$submit_info, {
+        source("myshinyapp/R/generateurs.R")
+        output$commentaires <- renderText({
+          generateur_demandeurs_d_emploi(input$annee, input$trimestre)})
+        output$tableau <- renderTable({
+          generateur_table_demandeurs_d_emploi(as.numeric(input$annee), as.numeric(input$trimestre), max(as.numeric(input$horizon),2))}, caption="Nombre de demandeurs d'emploi", digits = 0)
+        output$graphique1 <- renderPlotly(NULL)
+        output$graphique2 <- renderPlotly(NULL)
     })
+    } else if (input$paragraphe == "Créations d'entreprises") {
+      observeEvent(input$submit_info, {
+        source("myshinyapp/R/generateurs.R")
+        output$commentaires <- renderText({
+          generateur_creation_entreprise(input$annee, input$trimestre)})
+        output$tableau <- renderTable({
+          generateur_table_creation_entreprise(as.numeric(input$annee), as.numeric(input$trimestre), max(as.numeric(input$horizon),2))}, caption="Nombre de créations d'entreprises", digits = 0)
+        output$graphique1 <- renderPlotly(generateur_figure5(as.numeric(input$annee), as.numeric(input$trimestre), as.numeric(input$horizon)))
+        output$graphique2 <- renderPlotly(NULL)
+    })
+}
+})
 }
